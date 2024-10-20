27665b5b50e4db6746d2ce4e16dc38c363e0b787
Allana Diaz <99528220+ayzabelled@users.noreply.github.com>
2024-10-20 22:56:20 +0800

    Revert "Merged from main"
    This reverts commit 23b7b8d93b651c5f704ff31e9850a4aa7c8cfd2a, reversing
    changes made to c468b663b4a134f13195616301b8c7bb569733b8.

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-<<<<<<< HEAD
 # editable variables
 var speed := 75
 var accel := 5 
@@ -18,19 +17,10 @@ var has_item : bool
 @onready var interact_collision: CollisionShape2D = $InteractArea/InteractCollision
 @onready var ray_1: RayCast2D = $InteractArea/InteractCollision/Ray1
 
-=======
-const speed := 50
-const accel := 5
-
-@onready var player_sprite: Sprite2D = $PlayerSprite
-@onready var animation_player: AnimationPlayer = $PlayerSprite/AnimationPlayer
-@onready var animation_tree: AnimationTree = $PlayerSprite/AnimationTree
->>>>>>> main
 
 # this functions runs once 
 func _ready() -> void:
 	animation_player.play("idle_down")
-<<<<<<< HEAD
 	if self.has_node("Item") == false:
 		has_item = false
 		interact_collision.disabled = false
@@ -38,17 +28,13 @@ func _ready() -> void:
 		has_item = true
 		interact_collision.disabled = true
 	
-=======
-
->>>>>>> main
 # this functions runs every frame
 func _physics_process(delta: float) -> void:
 	
-	# gets input within the 4 axis (+x, -x, +y, -y)
+	# gets input within the 4 axis (-x, +x, -y, +y)
 	var input_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	
 	# movement formula
-<<<<<<< HEAD
 	movement(input_direction, delta)
 	
 	#interact
@@ -61,8 +47,6 @@ func _physics_process(delta: float) -> void:
 func movement(input_direction, delta) -> void:
 	
 	# movement smoothness formula
-=======
->>>>>>> main
 	velocity = lerp(velocity, input_direction * speed, delta * accel)
 
 	# do this if player stops moving
@@ -74,7 +58,6 @@ func movement(input_direction, delta) -> void:
 		elif animation_player.current_animation == "walk_up": animation_player.play("idle_up")
 		elif animation_player.current_animation == "walk_down": animation_player.play("idle_down")
 
-<<<<<<< HEAD
 	else:
 		if abs(input_direction.x) > abs(input_direction.y):
 			if input_direction.x > 0: animation_player.play("walk_right")
@@ -166,15 +149,3 @@ func _on_itemexited(area: Area2D) -> void:
 	if area.collision_layer == 8: detected_item.erase(area)
 	if area.collision_layer == 16: detected_station.erase(area)
 	
-=======
-	else: # if player is moving
-		animation_player.speed_scale = (( abs(velocity.x) + abs(velocity.y)) /30) # speeds animation via velocity
-		if Input.is_action_pressed("left") and not Input.is_action_pressed("right"): animation_player.play("walk_left")
-		elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"): animation_player.play("walk_right")
-		elif Input.is_action_pressed("up"): animation_player.play("walk_up")
-		elif Input.is_action_pressed("down"): animation_player.play("walk_down")
-	
-	
-	# when colliding with another collision, player slides.
-	move_and_slide()
->>>>>>> main
