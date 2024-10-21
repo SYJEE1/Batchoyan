ba0bb3d0fd4a336057adb5820a4825f9341cda83
erythgange <erythjohn.gange@gmail.com>
2024-10-21 14:53:17 +0800

    feat: colaref (wip)
    basic hotbar animations

--- Non_Entity/Stations/Stations/colaref.gd
+++ Non_Entity/Stations/Stations/colaref.gd
@@ -1,7 +1,34 @@
 extends Area2D
 
-var takes_item: bool = true
+var takes_item: bool = false
 var tilemap_glow : TileMapLayer
 
-func interact(carried_item) -> void:
-	pass
+const ITEM = preload("res://Non_Entity/Items/Item.tscn")
+const HOTBAR = preload("res://Non_Entity/Stations/Stations/hotbar.tscn")
+
+func _ready() -> void:
+	tilemap_glow.modulate = Color(1,1,1,0)
+
+func interact(carried_item):
+	
+	var hotbar_instance = HOTBAR.instantiate()
+	hotbar_instance.global_position = global_position
+	add_child(hotbar_instance)
+	
+	var instance = ITEM.instantiate()
+	instance.global_position = global_position
+	
+	# to water
+	instance.set_item(&"water", 1)
+	"res://Non_Entity/Stations/Stations/hotbar.tscn"
+	get_tree().root.add_child(instance)
+	return instance
+		
+func glow() -> void:
+	var glow_tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	glow_tween.tween_property(tilemap_glow, "modulate", Color(1,1,1,1), .1)
+	unglow(glow_tween)
+	
+func unglow(glow_tween) -> void:
+	if glow_tween.finished:
+		glow_tween.tween_property(tilemap_glow, "modulate", Color(1,1,1,0), .1)
