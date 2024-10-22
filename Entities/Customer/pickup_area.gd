948a4dd7643a7fead0175417f4b9c9543d3f2272
Allana Diaz <99528220+ayzabelled@users.noreply.github.com>
2024-10-23 00:17:33 +0800

    added pickup area

---
+++ Entities/Customer/pickup_area.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var has_item = Global.get_item_state()  # Flag to track if the player is holding an item
+var player_scene: PackedScene
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	Global.connect("item_state_changed", Callable(self, "_on_item_state_changed"))
+
+func _on_body_entered(body: RigidBody2D):
+	if body is RigidBody2D:
+		if is_item_matching(body):
+			if not has_item:
+				body.queue_free()  # Remove the item from the scene
+				get_parent().finish_order()
+			else:
+				print("Player is holding the item")
+		else:
+			print("Player is holding an incorrect item: ", body.name)
+
+func _on_item_state_changed(state: bool):
+	has_item = state  # Update the has_item state based on the player's signal
+
+func is_item_matching(item: RigidBody2D) -> bool:
+	return item.custom_item_type == "bowl_super"  # Example check, adjust as necessary
