e8192c8f3135bdf3236f1e5384fe47a15a5bbe45
Allana Diaz <99528220+ayzabelled@users.noreply.github.com>
2024-10-24 04:22:29 +0800

    Revert "Merge branch 'main' into interaction_updated"
    This reverts commit 83bd489e14c8ab29902999b41a3671e5b3163dd5, reversing
    changes made to cd202971cdd951f8d5c81bfc55e207a3f847eaf0.

--- Non_Entity/Tutorial/tutorial.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var fps_label: Label = $Camera2D/FPSLabel
-
-func _ready() -> void:
-	Engine.max_fps = 60
-
-func _process(delta: float) -> void:
-	fps_label.text = "FPS: %d" %Engine.get_frames_per_second()
