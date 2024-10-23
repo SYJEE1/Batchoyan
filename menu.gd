0a3f798bcc31b4b617925ab2d6c82f9a4cc1dd6c
SYJEE1 <166915640+SYJEE1@users.noreply.github.com>
2024-10-23 18:48:47 +0800

    fix menu scene
    this commit will fix the menu scene to match the canvas size and connect the button to the tutorial scene

--- menu.gd
+++ menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Environment/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Non_Entity/Tutorial/Tutorial.tscn")
 
 
 func _on_option_pressed():
